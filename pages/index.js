b205328948ce06871dec436aff2026a7b72e1eba
wagnersantanasoft <82161925+wagnersantanasoft@users.noreply.github.com>
2025-01-31 18:08:28 +0000

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Estou apredendo a trabalhar com o Git! Top...</h1>
+    return <h1>Estou apredendo a trabalhar com o Git! Top... Achei massa a integração com a Vercel.</h1>
 }
 
 export default Home;
